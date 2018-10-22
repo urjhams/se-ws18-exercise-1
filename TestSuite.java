5abf6f3052fc2ef24c4ca2871e65d06dde196db0
Darpan <darpan.vats21@gmail.com>
2018-10-22 15:23:50 +0200

    variable name changes

--- TestSuite.java
+++ TestSuite.java
@@ -5,22 +5,22 @@ public class TestSuite {
 
   @Test public static void ImageTest1() {
     Image i = new Image(100,100);
-    assert (i.data.length == 30000);
+    assert (i.imageData.length == 30000);
   }
 
   @Test public static void ImageTest2() {
     Image i = new Image(100,100);
     i.set(0,0,0x123456);
-    assert (i.data[0] == (byte)0x12);
-    assert (i.data[1] == (byte)0x34);
-    assert (i.data[2] == (byte)0x56);
-    assert (i.data[3] == (byte)0x00);
+    assert (i.imageData[0] == (byte)0x12);
+    assert (i.imageData[1] == (byte)0x34);
+    assert (i.imageData[2] == (byte)0x56);
+    assert (i.imageData[3] == (byte)0x00);
   }
 
   @Test public static void ImageTest3() {
     Image i = new Image(100,100);
     i.set(99,99,0x123456);
-    int len = i.data.length;
+    int len = i.imageData.length;
     assert (false);
   }
 
