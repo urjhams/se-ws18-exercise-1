fe30c794cf2da776954144547afc67a192e3251e
Darpan <darpan.vats21@gmail.com>
2018-10-22 14:54:25 +0200

    added functionalities to methods

--- Image.java
+++ Image.java
@@ -1,12 +1,11 @@
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.*;
 
 public class Image {
   public byte[] imageData = null ;
-  int width = 0;
-  int height = 0;
-  //private static String matrix="";
+  private int width = 0;
+  private int height = 0;
 
   public Image(int width, int height) {
     this.width = width;
@@ -26,14 +25,32 @@ public class Image {
           imageData [x + y * width] = (byte) val;
       }
     }
-  }
-
-  // write image data to file as image format
-
-  public void write( String filename) throws FileNotFoundException, IOException{
-    FileOutputStream fos = new FileOutputStream(filename);
-    fos.write(new String(imageData).getBytes());
-    fos.close();
 
   }
+
+  // write image data to file as image format P3 or P6
+
+  public void  write( String filename) throws IOException{
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename)));
+    // write header
+    int rowdimension = width;
+    int columndimension = height;
+    writer.write("P6");
+    writer.newLine();
+    writer.write(width+" "+height);
+    writer.newLine();
+    writer.write("256");
+    writer.newLine();
+    for(int row=0;row<rowdimension;row++){
+      for(int column=0;column<columndimension;column++){
+        writer.write(String.valueOf(imageData));
+        if(column < columndimension - 1)writer.write(" ");
+      }
+      writer.newLine();
+    }
+    writer.flush();
+    writer.close();
+  }
+
+
 }
